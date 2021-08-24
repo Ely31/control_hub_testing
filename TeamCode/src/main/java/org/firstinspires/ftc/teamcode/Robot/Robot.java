4d03d0eadde45a5f0b1e4a22a1ab6036b357da68
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-08-24 14:53:06 -0500

    mecanum code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Robot{
+
+    public DcMotor lf;
+    public DcMotor lb;
+    public DcMotor rf;
+    public DcMotor rb;
+
+    // constructor
+    public Robot(){
+
+    }
+
+    public void init(){
+
+    }
+
+}
